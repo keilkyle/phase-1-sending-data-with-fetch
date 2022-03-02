c72ccdd52921073cc8103897e7d891c32e758a3a
Christian Keil <christian@astranis.com>
2022-03-01 23:19:23 -0800

    commit tree b69097d4fded049a172258a07b5abe89f84f6f11

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData (name, email) {
+    const formData = {
+        "name": name,
+        "email": email 
+    }
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify(formData)
+    })
+    .then(resp => resp.json())
+    .then(data => {
+        const id = data.id
+        const p = document.createElement("p");
+        document.querySelector("#stuff").append(id, p)
+    })
+    .catch(message => {
+        const p = document.createElement("p");
+        document.querySelector("#stuff").append(message, p)})
+    
+    }
